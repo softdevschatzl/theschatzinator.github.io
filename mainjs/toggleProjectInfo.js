c7a745fda6507cdf8b02e7c52684fa16ab0de21f
theschatzinator <40133353+theschatzinator@users.noreply.github.com>
2023-04-23 12:51:01 -0400

    fixed fade-in property for project info

--- mainjs/toggleProjectInfo.js
+++ mainjs/toggleProjectInfo.js
@@ -2,14 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
   document.querySelectorAll('.project-item').forEach(function (projectItem) {
       projectItem.querySelector('.cloud').addEventListener('click', function () {
           const projectInfo = projectItem.querySelector('.project-info');
-          projectInfo.classList.toggle('show');
+          projectInfo.classList.toggle('visible');
       });
   });
   document.addEventListener('click', function (event) {
     const target = event.target;
     if (!target.closest('.project-info') && !target.closest('.cloud')) {
-        document.querySelectorAll('.project-info.show').forEach(function (projectInfo) {
-            projectInfo.classList.remove('show');
+        document.querySelectorAll('.project-info.visible').forEach(function (projectInfo) {
+            projectInfo.classList.remove('visible');
         });
     }
   });
