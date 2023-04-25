b5a2108c6226c78d2cbaaabb3d3a8a55d95fa581
theschatzinator <40133353+theschatzinator@users.noreply.github.com>
2023-04-25 13:19:01 -0400

    tried to fix image flip, tbc

--- mainjs/imageflip.js
+++ mainjs/imageflip.js
@@ -1,12 +1,18 @@
 document.getElementById("myImage").addEventListener("click", function() {
   const img = document.getElementById("myImage");
   img.classList.add("flip");
+
+  // Add animation to the image flip
+  img.style.transition = "transform 0.5s";
+  img.style.transform = "rotateY(180deg)";
+
   setTimeout(function() {
     const newSrc = (img.getAttribute("src") === "../images/mefancy.jpg") ? "../images/snowboard.PNG" : "../images/mefancy.jpg";
     img.setAttribute("src", newSrc);
     img.classList.remove("flip");
+
+    // Add animation to rotate the image back to its original position
+    img.style.transition = "transform 0.5s";
+    img.style.transform = "rotateY(0deg)";
   }, 500);
 });
-
-
-
